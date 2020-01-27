8d40349137ee3e056d11516bd4165b9cfd9754fc
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-01-27 22:16:36 +0000

    Stable version of OOP framework uploaded.

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import paperforest as pf
 
 import os
 import ads
+import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 
 def main():
 
     # Import your ADS token
-    ads.config.token = 'enter-your-token-here'
+    ads.config.token = 'yourToken'
 
     # Specify the paper to start from
     article_bib = '1985ApJS...57...77S'
@@ -16,20 +17,31 @@ def main():
     # Create tree and save as NetworkX graph if not existent
     if not os.path.isfile('./' + article_bib + '.gpickle'):
         tree = pf.Tree(start_paper = article_bib)
-        tree.build_reference_tree(depth = 1)
+        tree.build_reference_tree(depth = 2)
         tree.save(article_bib)
 
-    # Pickle-back the graph and plot it
+    # Pickle-back the graph
     G = nx.read_gpickle(article_bib + '.gpickle')
-    layout = nx.spring_layout(G, iterations=50)
-    nx.draw(G, layout,
-            with_labels=True, node_color='orange',
-            node_size=400, edge_color='black', linewidths=1,
-            font_size=7)
 
+    # Plot the graph
+    pos = nx.spring_layout(G)
+    nx.draw_networkx_nodes(G, pos, node_size=3, node_color='orange')
+    nx.draw_networkx_edges(G, pos, alpha=0.3, edge_color='lime')
     plt.axis('off')
-    plt.savefig(article_bib + '.png', dpi = 300)
-    plt.show()
+    plt.savefig('graph_' + article_bib + '.png', dpi = 300)
+
+
+    # Plot the degree distribution
+    plt.clf()
+    degree_sequence = sorted([d for n, d in G.degree()], reverse=True)  # degree sequence
+    bins = np.logspace(0, 3, 20)
+    plt.hist(degree_sequence, bins=bins, color='b')
+    plt.title("Degree Histogram")
+    plt.ylabel("Count")
+    plt.xlabel("Degree")
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.savefig('degree_distribution_' + article_bib + '.png', dpi = 250)
 
 
 if __name__ == '__main__':
